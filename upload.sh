9659736aef7242f43fd6d3a66ae2c1a45cc4dc8f
Sungjoon Moon <sumoon@seoulsaram.org>
2025-06-24 04:20:42 +0900

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,2 +1,2 @@
 php gen.php
-rsync -avPuh --exclude='*.md' --exclude='.git' --exclude='**/*.php' --exclude='**/*.py' --exclude='**/*.sh' . cutestpoo@seoulsaram.org:/usr/share/caddy/seoulsaram.org
+rsync -avPuh --exclude='**/*.md' --exclude='.git' --exclude='**/*.php' --exclude='**/*.py' --exclude='**/*.sh' . cutestpoo@seoulsaram.org:/usr/share/caddy/seoulsaram.org
